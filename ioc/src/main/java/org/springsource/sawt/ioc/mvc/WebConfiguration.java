9c4791a466f9311a7eae1c87d9c06eb382ac4143
Josh Long <josh@joshlong.com>
2012-04-08 01:31:36 -0500

    compiling..

--- ioc/src/main/java/org/springsource/sawt/ioc/mvc/WebConfiguration.java
+++
@@ -1,9 +0,0 @@
-package org.springsource.sawt.ioc.mvc;
-
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-
-@EnableWebMvc
-public class WebConfiguration extends WebMvcConfigurerAdapter {
-
-}
---
+++ web/mvc/src/main/java/org/springsource/examples/sawt/web/mvc/MvcWebApplicationInitializer.java
@@ -0,0 +1,11 @@
+package org.springsource.examples.sawt.web.mvc;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: jlong
+ * Date: 4/8/12
+ * Time: 12:57 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class MvcWebApplicationInitializer {
+}
