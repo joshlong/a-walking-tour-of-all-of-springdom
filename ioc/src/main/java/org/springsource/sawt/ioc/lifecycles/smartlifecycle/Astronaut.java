11c6fa08373247503df74993ca0982f14c2c3bda
Josh Long <josh@joshlong.com>
2013-04-24 18:36:45 -0700

    updated lifecycle code.

--- ioc/src/main/java/org/springsource/sawt/ioc/lifecycles/smartlifecycle/Astronaut.java
+++ ioc/src/main/java/org/springsource/sawt/ioc/lifecycles/smartlifecycle/Astronaut.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public class Astronaut implements SmartLifecycle {
+
     private Log log = LogFactory.getLog(getClass());
 
     private volatile boolean running = false;
