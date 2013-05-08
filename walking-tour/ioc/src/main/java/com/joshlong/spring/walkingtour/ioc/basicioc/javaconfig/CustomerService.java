499171e8bbe760622b2613a2183e09b768ee56f0
Josh Long <josh@joshlong.com>
2013-05-08 11:56:22 -0700

    updating the configuration.

--- walking-tour/ioc/src/main/java/com/joshlong/spring/walkingtour/ioc/basicioc/javaconfig/CustomerService.java
+++ walking-tour/ioc/src/main/java/com/joshlong/spring/walkingtour/ioc/basicioc/javaconfig/CustomerService.java
@@ -4,15 +4,12 @@ import org.apache.commons.lang.builder.ToStringBuilder;
 
 import javax.sql.DataSource;
 
-/**
- * Example class that requires a {@link javax.sql.DataSource}
- */
+
 public class CustomerService {
 
     private DataSource dataSource;
 
     public void setDataSource(DataSource ds) {
         this.dataSource = ds;
-        System.out.println("the data source has been set " + ToStringBuilder.reflectionToString(this.dataSource));
     }
 }
--- walking-tour/ioc/src/main/java/com/joshlong/spring/walkingtour/ioc/basicioc/qualifiers/Main.java
+++ walking-tour/ioc/src/main/java/com/joshlong/spring/walkingtour/ioc/basicioc/qualifiers/Main.java
@@ -3,8 +3,9 @@ package com.joshlong.spring.walkingtour.ioc.basicioc.qualifiers;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Main {
-    static public void main(String args[]) throws Throwable {
-        AnnotationConfigApplicationContext ac =  new AnnotationConfigApplicationContext(ApplicationConfiguration.class);
-        ac.registerShutdownHook();
-    }
+	public static void main(String args[]) throws Throwable {
+		AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(
+				ApplicationConfiguration.class);
+		ac.registerShutdownHook();
+	}
 }
